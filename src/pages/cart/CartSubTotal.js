fd09d2696933bb3caaf145fff8396bea3b7e0074
lamilekan263 <lamilekan263@gmail.com>
2020-12-17 13:22:26 +0100

    added few features

--- src/pages/cart/CartSubTotal.js
+++ src/pages/cart/CartSubTotal.js
@@ -1,33 +1,56 @@
-import React from 'react'
+import React  from 'react'
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from '../../redux/basket/basketReducer';
 
 import Button from "../../components/button/Button.js";
 import paymentImage from "../../Assets/payment.png";
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-const CartSubTotal = ({basketItems}) => {
-    return (
-      <div className="cart__payment d-flex flex-column align-items-center my-5">
-        <CurrencyFormat
-          renderText={(value) => (
-            <>
-              <p>Subtotal({basketItems?.length}):</p>
-              <strong>{value}</strong>
-              <small className="subtotal__gift">
-                <input type="checkbox" /> This order contains a gift
-              </small>
-            </>
-          )}
-          decimalScale={2}
-          value={getBasketTotal(basketItems)}
-          displayType={"text"}
-          thousandSeparator={true}
-          prefix={"$"}
-        />
-        <Button button="PROCEED TO PAYMENT" buttonType="buttonBlack" />
-        <img src={paymentImage} alt="payment logo" className="mt-3" />
-      </div>
-    );
+const CartSubTotal = ({ basketItems, user }) => {
+  const history = useHistory();
+
+  const handlePayment = () => {
+    if (user === null) {
+      alert('Please Sign in to pay for your Goods')
+      history.push('/user/signin')
+    } else {
+      history.push('/checkout')
+    }
+  };
+  return (
+    <div className="cart__payment d-flex flex-column align-items-center my-5">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>Subtotal({basketItems?.length}):</p>
+            <strong>{value}</strong>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basketItems)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      <Button
+        button="PROCEED TO PAYMENT"
+        buttonType="buttonBlack"
+        btnClicked={handlePayment}
+      />
+      <img src={paymentImage} alt="payment logo" className="mt-3" />
+    </div>
+  );
+};
+
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+  };
 }
 
-export default CartSubTotal
+export default connect(mapStateToProps)(CartSubTotal);
