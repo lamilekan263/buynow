a97d74b4068b606a6948bc9e2fada0ac8e05f03f
lamilekan263 <lamilekan263@gmail.com>
2020-12-29 13:47:35 +0100

    fixed the wishlist page

--- src/components/Wishlist/Wishlist.js
+++ src/components/Wishlist/Wishlist.js
@@ -3,22 +3,41 @@ import { connect } from "react-redux";
 import { removeFromFavorites } from '../../redux/favorite/favoritesAction';
 import WishlistCard from './wishListCard/WishlistCard';
 
+import notwishlist from '../../Assets/wishlist2.png'
 
 const Wishlist = ({ wishlist, removeFromFavorites }) => {
   return (
-    <div className="row">
-      {wishlist.map((wishes) => {
-        return (
-          <div className="col-md-4" key={wishes.id}>
-            <WishlistCard
-              imageSrc={wishes.imageSrc}
-              removeFromFavorites={removeFromFavorites}
-              id={wishes.id}
-              name={wishes.name}
+    <div>
+      {wishlist.length === 0 ? (
+        <div className="row justify-content-center">
+          <div style={{ height: "300px" }}>
+            <img
+              src={notwishlist}
+              alt=""
+              className="col-12"
+              style={{ height: "100%" }}
             />
           </div>
-        );
-      })}
+          <h4 style={{ textAlign: "center" }} className="col-12">
+            Sorry you haven't added to your wishlist for santa
+          </h4>
+        </div>
+      ) : (
+        <div className="row">
+          {wishlist.map((wishes) => {
+            return (
+              <div className="col-md-4">
+                <WishlistCard
+                  imageSrc={wishes.imageSrc}
+                  removeFromFavorites={removeFromFavorites}
+                  id={wishes.id}
+                  name={wishes.name}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
